023e3323d86676f7a18fc34c9ba50d62344e93e8
totetero <totetero@gmail.com>
2026-06-15 00:19:44 +0900

    コンテナ作成時に自動化設定を追加

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+# claudeの設定ファイルを作成
+mkdir -p ~/.claude
+cat <<EOF > ~/.claude/settings.json
+{
+  "permissions": {
+    "defaultMode": "auto"
+  }
+}
+EOF
+
 # chromiumのインストール
 npm install
 npx -y playwright install --with-deps chromium
